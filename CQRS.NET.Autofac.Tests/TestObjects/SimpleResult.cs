1084d02e86cde84c43d85c9f5380c0c742625c91
Ravy Amiry <982350+ravy-amiry@users.noreply.github.com>
2020-11-03 01:39:14 +0330

    Move Autofac parts to another solution

--- CQRS.NET.Autofac.Tests/TestObjects/SimpleResult.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-namespace CQRS.NET.Autofac.Tests.TestObjects {
-    public class SimpleResult {
-        public CallChain CallChain { get; set; } = new CallChain();
-        public Type GeneratedBy { get; internal set; }
-    }
-}
